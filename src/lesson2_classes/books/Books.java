d5e7eb2d2ec5609ecb47cd56958744aea9f5a3e0
SvetSpace <svetikt2001@gmail.com>
2022-07-15 08:03:20 +0800

    +

---
+++ src/lesson2_classes/books/Books.java
@@ -0,0 +1,68 @@
+package lesson2_classes.books;
+
+public class Books {
+    private String name;
+    private String author;
+    private int id;
+    private int year;
+    private int pages;
+    private Section section;
+
+    public Books() {
+        this.name = name;
+        this.author = author;
+        this.id = id;
+        this.year = year;
+        this.pages = pages;
+        this.section = section;
+    }
+
+    String getInfo() {
+        String info = "";
+        info += "Название             " + name + "\n";
+        info += "Автор                " + author + "\n";
+        info += "Регистрационный номер" + id + "\n";
+        info += "Год издания          " + year + "\n";
+        info += "Кол-во страниц       " + pages + "\n";
+        info += "Раздел               " + section.sectionName + "\n";
+
+        System.out.println("Добавьте книгу");
+        System.out.println("Введите название:");
+        return info;
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
+    public void add(Books books) {
+    }
+}
+    enum Section
+    {
+        SCIENCE("Наука"), FANTASTIC("Фантастика") ;
+        String sectionName;
+        Section (String section) {this.sectionName = section;}
+    }
+
---
+++ src/lesson2_classes/books/Reader.java
@@ -0,0 +1,16 @@
+package lesson2_classes.books;
+
+import java.util.Date;
+
+public class Reader {
+    private String name;
+    private String address;
+    private String phone;
+
+    public Reader (String name, String address, String phone)
+    {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+}
